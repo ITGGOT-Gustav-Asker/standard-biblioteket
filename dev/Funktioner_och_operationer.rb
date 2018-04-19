3fa9278649e15f24e22614231c4acae756210ce7
ITGGOT-Gustav-Asker <38528632+ITGGOT-Gustav-Asker@users.noreply.github.com>
2018-04-19 14:41:58 +0200

    bytte namn

--- dev/Funktioner_och_operationer.rb
+++ dev/Funktioner_och_operationer.rb
@@ -1,3 +1,7 @@
+def add(term1, term2)
+    return term1 + term2
+end
+
 def prev_num(num1,num2)
 return num1 - num2
 end
@@ -9,4 +13,3 @@ end
 def cube(num)
     return num ** num * num
 end
-
--- lib/add.rb
+++
@@ -1,4 +0,0 @@
-def add(term1, term2)
-    return term1 + term2
-end
-
