5afe1456d3ee9a7ab50103351d66c26d36c25e02
ITGGOT-Gustav-Asker <38528632+ITGGOT-Gustav-Asker@users.noreply.github.com>
2018-04-26 14:37:41 +0200

    TOMDOC+ fnktionen

---
+++ lib/next_num.rb
@@ -0,0 +1,12 @@
+#Public: Takes a number and adds one to the given number
+#
+#num1 = number who you want to add one too
+#
+#example: 5
+#=> 6
+#
+#Returns the next number from your given number.
+
+def prev_num(num1)
+    return num1 + 1
+end
