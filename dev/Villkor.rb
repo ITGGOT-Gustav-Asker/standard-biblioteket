f15d5f4de66dc684ec251ab7b0ae42fdb7758bed
ITGGOT-Gustav-Asker <38528632+ITGGOT-Gustav-Asker@users.noreply.github.com>
2018-04-26 13:52:19 +0200

    Gjorde grejer
    Fixade funktionen

--- dev/Villkor.rb
+++ dev/Villkor.rb
@@ -26,3 +26,10 @@ def between(num1, num2, num3)
     end
 end
 
+def between_strict(num1, num2, num3)
+    if num1 > num2 && num1 < num3
+        return true
+    else
+        return false
+    end
+
