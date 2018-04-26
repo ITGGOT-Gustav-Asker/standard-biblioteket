af35f63c41135e91bca77fbe81a41817db5d5568
ITGGOT-Gustav-Asker <38528632+ITGGOT-Gustav-Asker@users.noreply.github.com>
2018-04-26 14:07:46 +0200

    TomDoc

--- lib/absolute.rb
+++ lib/absolute.rb
@@ -1,3 +1,14 @@
+#Pulic: Takes one integer and returns the absolute version of the number
+#
+#num = the number who will become absolute
+#
+#example: -1 
+# => 1
+#example2: 5
+#=> 5
+#
+#return the positive version of a given number
+
 def absolute(num)
     if num < 0
         num = num * -1
