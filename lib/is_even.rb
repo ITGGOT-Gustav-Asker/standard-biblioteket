09f0919f4ed411327017cfc53b1c00b069a2717f
ITGGOT-Gustav-Asker <38528632+ITGGOT-Gustav-Asker@users.noreply.github.com>
2018-04-26 14:18:54 +0200

    TOMDOC

--- lib/is_even.rb
+++ lib/is_even.rb
@@ -1,3 +1,14 @@
+#Public: Checks if a number is even
+#
+#num = the number you want to check if it's even
+#
+#example: 2
+#=> true
+#example2: 3
+#=> false
+#
+#Return true if the number is evens
+
 def is_even(num)
     return(num % 2) == 0
 end
