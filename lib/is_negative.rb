97bb7e9463b630cdf2934d1c596136d65c5959b8
ITGGOT-Gustav-Asker <38528632+ITGGOT-Gustav-Asker@users.noreply.github.com>
2018-04-26 14:21:11 +0200

    TOMDOC

--- lib/is_negative.rb
+++ lib/is_negative.rb
@@ -1,3 +1,14 @@
+#Public: Checks if a number is negative
+#
+#num = The number you want to check if it's negative
+#
+#example: -1
+#=> true
+#example2: 1
+#=> false
+#
+#Return true if the number is <0 = negative
+
 def is_negative(num)
     
     return num < 0 
