1e21b33879fd5facbb6920e9adc06585e16411d2
ITGGOT-Gustav-Asker <38528632+ITGGOT-Gustav-Asker@users.noreply.github.com>
2018-04-26 14:26:19 +0200

    tomdoc

--- lib/prev_num.rb
+++ lib/prev_num.rb
@@ -1,3 +1,12 @@
-def prev_num(num1,num2)
-    return num1 - num2
+#Public: Takes a numer and subtracts one from the given number
+#
+#num1 = number who you want to subtract one from
+#
+#example: 5
+#=> 4
+#
+#Returns the previous number from your given number.
+
+def prev_num(num1)
+    return num1 - 1
 end
