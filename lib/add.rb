ba5c17ac161926a5d553dd02a821392254cefde4
ITGGOT-Gustav-Asker <38528632+ITGGOT-Gustav-Asker@users.noreply.github.com>
2018-04-26 14:04:56 +0200

    La till TomDoc

--- lib/add.rb
+++ lib/add.rb
@@ -1,3 +1,13 @@
+#Public: Add 2 integers together.
+#
+#term1 = One of the numbers that will be added together
+#
+#term2 = the number who will be added to the first number
+#
+#example 3 + 4 
+# => 7
+# Returns number added together.
+
 def add(term1, term2)
     return term1 + term2
 end
