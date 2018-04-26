999e018b33fd9dcc34f01949b8c59a3a30d20764
ITGGOT-Gustav-Asker <38528632+ITGGOT-Gustav-Asker@users.noreply.github.com>
2018-04-26 14:27:55 +0200

    tomdoc

--- lib/square.rb
+++ lib/square.rb
@@ -1,3 +1,11 @@
+#Public: Takes one number and takes it square itself 
+#
+#num = The number you want to square
+#
+#example: 5
+#=> 25
+#
+#Returns the number square itself, num^2
 def square_(num)
     return num ** num 
 end
