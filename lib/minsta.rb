303d27e8b56f0ec8930757ba951d9656e4d48e99
ITGGOT-Gustav-Asker <38528632+ITGGOT-Gustav-Asker@users.noreply.github.com>
2018-04-26 14:24:07 +0200

    tomdoc

--- lib/minsta.rb
+++ lib/minsta.rb
@@ -1,3 +1,13 @@
+#Public: Checks the smallest number based on two numbers
+#
+#num1= a number
+#num2= number who you compare with
+#
+#example: 2,3
+#=> 2
+#
+#Returns the smallest of the numbers
+
 def minsta(num1, num2)
     if num1 < num2
         return num1
