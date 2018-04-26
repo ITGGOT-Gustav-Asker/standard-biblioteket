1ae99e20aa130dbe25c39aa7018ffe2937c8a697
ITGGOT-Gustav-Asker <38528632+ITGGOT-Gustav-Asker@users.noreply.github.com>
2018-04-26 14:13:44 +0200

    TOMDOC

--- lib/between.rb
+++ lib/between.rb
@@ -1,3 +1,20 @@
+#Pulic: Takes three numbers and checks if num2 is a number somewhere between the two others or the same number
+#
+#num1 = first number 
+#
+# num2 = second number
+#
+# num3 = third number
+#
+#example: -1,-1,-3 
+# => true
+#example2: 1,2,3
+#=> true
+#example3: 1,-1,2
+#=> false
+#
+#Returns true if the number is between the other two numbers or the same number as one of the others.
+
 def between(num1, num2, num3)
     if num1 >= num2 && num1 <= num3
         return true
