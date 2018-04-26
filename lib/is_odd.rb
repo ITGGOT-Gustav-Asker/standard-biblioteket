abac5c4b0478315c6793d6a596e2eca1d1bd6df4
ITGGOT-Gustav-Asker <38528632+ITGGOT-Gustav-Asker@users.noreply.github.com>
2018-04-26 14:22:14 +0200

    tomdoc

--- lib/is_odd.rb
+++ lib/is_odd.rb
@@ -1,3 +1,14 @@
+#Public: Checks if a number is odd
+#
+#num = the number you want to check if it's odd
+#
+#example: 2
+#=> false
+#example2: 3
+#=> true
+#
+#Return true if the number is odd
+
 def is_odd(num)
     return(num % 2) == 1
 end
