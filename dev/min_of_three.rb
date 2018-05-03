10010a424f1639b30965a99138ebec96846c4811
ITGGOT-Gustav-Asker <38528632+ITGGOT-Gustav-Asker@users.noreply.github.com>
2018-05-03 13:18:46 +0200

    DID CODE AND TOMDOC

--- dev/min_of_three.rb
+++ dev/min_of_three.rb
@@ -1,9 +1,26 @@
-def min_of_three(num1,num2,num3)
-    if num1 < num 2
-        x = num1
-    elsif num1 > num 2
-        x = num2
+#Public: Figures out which number out of three, which is the smallest. 
+#
+#min = the variable who we guess is minimal and if not, changes to the smallest number.
+#num2 = the number we compare with
+#num3 = another number we compare with. 
+#
+#example: (1,2,3)
+#=> 1
+#
+#Returns the smallest number out of three different ones. 
+
+
+
+
+def min_of_three(min,num2,num3)
+    if min > num2
+        min = num2
     end
+    if min > num3
+        min = num3
+    end
+    return min
 end
 
-puts min_of_three(1,2,3)
+
+puts min_of_three(-1,-2,-2)
