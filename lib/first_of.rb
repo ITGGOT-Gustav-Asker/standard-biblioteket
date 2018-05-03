5a91e44e470fbe48f78a0e402f4b1022db63108d
ITGGOT-Gustav-Asker <38528632+ITGGOT-Gustav-Asker@users.noreply.github.com>
2018-05-03 14:38:22 +0200

    CODE AND TOMDOC, CHECK

---
+++ lib/first_of.rb
@@ -0,0 +1,14 @@
+#Public: Takes the first stuff in an array
+#
+#array = the list of stuff
+#
+#example: [1,2,3]
+#=> 1
+#
+#Returns the first stuff in a list(array)
+
+def first_of(array)
+    return array[0]
+end
+
+puts first_of([1,2,3,4])
---
+++ lib/last_of.rb
@@ -0,0 +1,15 @@
+#Public: Takes the last stuff in an array
+#
+#array = the list of stuff
+#
+#example: [1,2,3]
+#=> 3
+#
+#Returns the last stuff in a list(array)
+
+def last_of(array)
+
+    return array[-1]
+end
+
+puts last_of([2,3,4,1])
